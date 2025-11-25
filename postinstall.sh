f13b4214f0ac45ad6a7e0cb9fc495499047920fa
Nouvistiardi Azra <13176878+nouvist@users.noreply.github.com>
2025-11-25 09:35:39 +0700

    feat: use latest android platform

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,7 @@ export ANDROID_HOME="/opt/android/sdk"
 export PATH="\$PATH:/opt/flutter/bin"
 export PATH="\$PATH:/opt/android/sdk/cmdline-tools/latest/bin"
 export PATH="\$PATH:/opt/android/sdk/platform-tools"
-export PATH="\$PATH:/opt/android/sdk/build-tools/34.0.0"
+export PATH="\$PATH:/opt/android/sdk/build-tools/36.1.0"
 EOF
 
 echo 'Add environment to ~/.profile...'
@@ -15,8 +15,18 @@ echo 'Add environment to ~/.zprofile...'
 echo "source '/home/vscode/.environment'" >> '/home/vscode/.zprofile'
 source '/home/vscode/.environment'
 
-echo 'Installing Android Platform 33...'
-yes | sdkmanager 'platform-tools' 'platforms;android-33' 'build-tools;34.0.0'
+echo 'Installing Android Platform 34...'
+yes | sdkmanager 'platforms;android-34'
+
+echo 'Installing Android Platform 35...'
+yes | sdkmanager 'platforms;android-35'
+
+echo 'Installing Android Platform 36...'
+yes | sdkmanager 'platforms;android-36'
+
+echo 'Installing tools...'
+yes | sdkmanager 'platform-tools' 'build-tools;35.0.0' 'build-tools;36.1.0'
+
 flutter config --android-sdk '/opt/android/sdk'
 yes | flutter doctor --android-licenses
 
